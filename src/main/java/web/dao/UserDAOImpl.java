99fdbc1680139a3ac7478432f8f9bba4d249ee0b
ELMAN SELIMOV <elman221199@gmail.com>
2022-11-08 17:31:19 +0300

    the first

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -1,65 +1,43 @@
 package web.dao;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
-import static org.hibernate.internal.util.collections.CollectionHelper.isEmpty;
 
-
-@Component
+@Repository
 public class UserDAOImpl implements UserDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional(readOnly = true)
     public List<User> getUser() {
         List<User> user = entityManager.createQuery("SELECT c FROM User c").getResultList();
         return user;
     }
 
-    @Transactional(readOnly = true)
     public User getUser(Long id) {
-        Query query = entityManager.createQuery("select user from User user where user.id = :id");
-        query.setParameter("id", id);
-        List<User> user = query.getResultList();
-        if (!isEmpty(user)) {
-            return user.get(0);
-        }
-        return null;
+        return entityManager.find(User.class, id);
     }
 
-    @Transactional()
     public void save(User user) {
-        if (user.getId() == null) {
-            entityManager.persist(user);
-        } else {
-            entityManager.merge(user);
-        }
-        System.out.println("Contact saved with id: " + user.getId());
+        entityManager.persist(user);
     }
 
-    @Transactional()
     public void update(Long id, User updatedUser) {
-        Query query = entityManager.createQuery("UPDATE User user SET user.firstName = :firstName, user.lastName = :lastName, user.email = :email WHERE user.id = :id");
-        query.setParameter("id", id);
-        query.setParameter("firstName", updatedUser.getFirstName());
-        query.setParameter("lastName", updatedUser.getLastName());
-        query.setParameter("email", updatedUser.getEmail());
-        query.executeUpdate();
+        User user = entityManager.find(User.class, id);
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setEmail(updatedUser.getEmail());
+        entityManager.merge(user);
     }
 
-    @Transactional()
     public void delete(Long id) {
-        Query query = entityManager.createQuery("DELETE FROM User user WHERE user.id = :id");
-        query.setParameter("id", id);
-        query.executeUpdate();
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
     }
 
 }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,21 +2,25 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDAO;
 import web.model.User;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDAO userDAO;
 
+    @Transactional(readOnly = true)
     public List<User> getUser() {
         return userDAO.getUser();
     }
 
+    @Transactional(readOnly = true)
     public User getUser(Long id) {
         return userDAO.getUser(id);
     }
